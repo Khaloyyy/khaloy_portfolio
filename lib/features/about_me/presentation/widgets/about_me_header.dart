0f3e0e171b54b1e10eb72f6c92bed2e2afc5f5ce
Khaloyy <carlo.cabbo09@gmail.com>
2025-04-14 13:01:34 +0800

    Create about_me_header.dart

---
+++ lib/features/about_me/presentation/widgets/about_me_header.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+
+// import '../../../../../../core/common/styles/utol_text_styles.dart';
+
+// class AuditLogsHeader extends StatelessWidget {
+//   const AuditLogsHeader({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: <Widget>[
+//         Text(
+//           'Audit Logs',
+//           style: UtolTextStyles.title3BoldSp,
+//         ),
+//       ],
+//     );
+//   }
+// }
