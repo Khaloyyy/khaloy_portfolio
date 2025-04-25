63da499d6041f53b63c25b87655d8e2af7edee5f
Khaloyy <carlo.cabbo09@gmail.com>
2025-04-25 10:37:06 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:supertokens_flutter/supertokens.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'app.dart';
-import 'core/config/env/env.dart';
 
 //
 void main() async {
@@ -14,7 +12,6 @@ void main() async {
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // =============== Load Super Token =============== //
-  SuperTokens.init(apiDomain: Environment.apiUrl, apiBasePath: '/api/auth');
 
   setPathUrlStrategy();
 
